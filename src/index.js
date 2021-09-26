c616e270f32dd6e2e8883132ddfb7956e232013c
KxngJ <jaheimcbrown@gmail.com>
2021-09-26 18:28:32 -0500

    fix #3 ghpages

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename="Google-Homepage-Clone/">
+    <Router>
       <App />
     </Router>
   </React.StrictMode>,
