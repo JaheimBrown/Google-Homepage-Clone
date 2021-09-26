1523b759ffc9216d2313960e68af512150691af6
KxngJ <jaheimcbrown@gmail.com>
2021-09-26 18:33:38 -0500

    gh-pages fix omit

--- src/App.js
+++ src/App.js
@@ -10,13 +10,7 @@ function App() {
   return (
     <>
       <Navbar />
-      <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/about'exact component={About} />
-        <Route path='/store'exact component={Store} />
-        <Route path='/gmail'exact component={Gmail} />
-        <Route path='/images'exact component={Images} />
-      </Switch>
+      <Home />
     </>
   );
 }
